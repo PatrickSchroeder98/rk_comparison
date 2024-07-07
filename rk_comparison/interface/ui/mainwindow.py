949c18f64cd604d52ddbe38376c563e48f83918c
Patrick Schröder <patrickschroder479@gmail.com>
2024-07-07 13:48:36 +0200

    Created UI classes with inheritance from pyuic generated files

--- rk_comparison/interface/ui/mainwindow.py
+++ rk_comparison/interface/ui/mainwindow.py
@@ -1,49 +1,26 @@
-from PyQt6 import QtCore, QtGui, QtWidgets
-from rk_comparison.interface.ui.plottingwindow import Ui_PlottingWindow
-from rk_comparison.interface.ui.inputwindow import Ui_InputWindow
+from rk_comparison.interface.design.ui_mainwindow import Ui_MainWindow
+from rk_comparison.interface.ui.inputwindow import InputWindow
+from rk_comparison.interface.ui.plottingwindow import PlottingWindow
+from rk_comparison.core.controller.controller import Controller
+from PyQt6 import QtWidgets
 
-class Ui_MainWindow(object):
-    def setupUi(self, MainWindow, controller):
-        self.controller = controller
 
-        MainWindow.setObjectName("MainWindow")
-        MainWindow.resize(243, 286)
-        self.centralwidget = QtWidgets.QWidget(parent=MainWindow)
-        self.centralwidget.setObjectName("centralwidget")
-        self.input_button = QtWidgets.QPushButton(parent=self.centralwidget)
-        self.input_button.setGeometry(QtCore.QRect(60, 40, 121, 51))
-        self.input_button.setObjectName("input_button")
-        self.calculate_button = QtWidgets.QPushButton(parent=self.centralwidget)
-        self.calculate_button.setGeometry(QtCore.QRect(60, 120, 121, 51))
-        self.calculate_button.setObjectName("calculate_button")
-        self.exit_button = QtWidgets.QPushButton(parent=self.centralwidget)
-        self.exit_button.setGeometry(QtCore.QRect(60, 200, 121, 51))
-        self.exit_button.setObjectName("exit_button")
-        MainWindow.setCentralWidget(self.centralwidget)
-        self.statusbar = QtWidgets.QStatusBar(parent=MainWindow)
-        self.statusbar.setObjectName("statusbar")
-        MainWindow.setStatusBar(self.statusbar)
+class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
 
-        self.retranslateUi(MainWindow)
-        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+    def __init__(self, parent=None):
+        super(MainWindow, self).__init__(parent=parent)
+        self.setupUi(self)
 
-        self.MainWindow = MainWindow
+        self.controller = Controller()
         self.input_button.clicked.connect(self.input_clicked)
         self.calculate_button.clicked.connect(self.calculate_clicked)
         self.exit_button.clicked.connect(self.exit_clicked)
 
-    def retranslateUi(self, MainWindow):
-        _translate = QtCore.QCoreApplication.translate
-        MainWindow.setWindowTitle(_translate("MainWindow", "RK Comparison"))
-        self.input_button.setText(_translate("MainWindow", "Input Data"))
-        self.calculate_button.setText(_translate("MainWindow", "Calculate"))
-        self.exit_button.setText(_translate("MainWindow", "Exit"))
-
     def input_clicked(self):
         self.popup = QtWidgets.QDialog()
-        self.popup_ui = Ui_InputWindow()
-        self.popup_ui.setupUi(self.popup, self.controller)
-        self.popup.show()
+        self.popup_ui = InputWindow()
+        self.popup_ui.initialize_controller(self.controller)
+        self.popup_ui.exec()
 
     def calculate_clicked(self):
         self.controller.initialize(self.controller.id.get_t_min(),
@@ -57,9 +34,9 @@ class Ui_MainWindow(object):
         self.controller.compare()
 
         self.popup = QtWidgets.QDialog()
-        self.popup_ui = Ui_PlottingWindow()
-        self.popup_ui.setupUi(self.popup, self.controller)
-        self.popup.show()
+        self.popup_ui = PlottingWindow()
+        self.popup_ui.initialize_controller(self.controller)
+        self.popup_ui.show()
 
     def exit_clicked(self):
-        self.MainWindow.close()
+        self.close()
--- rk_comparison/interface/ui/plottingwindow.py
+++ rk_comparison/interface/ui/plottingwindow.py
@@ -1,36 +1,19 @@
-from PyQt6 import QtCore, QtGui, QtWidgets
+from rk_comparison.interface.design.ui_plottingwindow import Ui_PlottingWindow
+from PyQt6 import QtWidgets
 
 
-class Ui_PlottingWindow(object):
-    def setupUi(self, PlottingWindow, controller):
-        self.controller = controller
+class PlottingWindow(QtWidgets.QDialog, Ui_PlottingWindow):
 
-        PlottingWindow.setObjectName("PlottingWindow")
-        PlottingWindow.resize(278, 278)
-        self.plot_result = QtWidgets.QPushButton(parent=PlottingWindow)
-        self.plot_result.setGeometry(QtCore.QRect(70, 30, 141, 51))
-        self.plot_result.setObjectName("plot_result")
-        self.plot_comparison = QtWidgets.QPushButton(parent=PlottingWindow)
-        self.plot_comparison.setGeometry(QtCore.QRect(70, 110, 141, 51))
-        self.plot_comparison.setObjectName("plot_comparison")
-        self.cancel_button = QtWidgets.QPushButton(parent=PlottingWindow)
-        self.cancel_button.setGeometry(QtCore.QRect(70, 190, 141, 51))
-        self.cancel_button.setObjectName("cancel_button")
+    def __init__(self, parent=None):
+        super(PlottingWindow, self).__init__(parent=parent)
+        self.setupUi(self)
 
-        self.retranslateUi(PlottingWindow)
-        QtCore.QMetaObject.connectSlotsByName(PlottingWindow)
-
-        self.PlottingWindow = PlottingWindow
         self.plot_result.clicked.connect(self.plot_results_clicked)
         self.plot_comparison.clicked.connect(self.plot_comparison_clicked)
         self.cancel_button.clicked.connect(self.cancel_clicked)
 
-    def retranslateUi(self, PlottingWindow):
-        _translate = QtCore.QCoreApplication.translate
-        PlottingWindow.setWindowTitle(_translate("PlottingWindow", "Plotting"))
-        self.plot_result.setText(_translate("PlottingWindow", "Plot result"))
-        self.plot_comparison.setText(_translate("PlottingWindow", "Plot comparison"))
-        self.cancel_button.setText(_translate("PlottingWindow", "Cancel"))
+    def initialize_controller(self, controller):
+        self.controller = controller
 
     def plot_results_clicked(self):
         self.controller.plot(True, self.controller.results_rk, "Nuclear Decay [nuclei]", "Nuclear Decay")
@@ -39,4 +22,4 @@ class Ui_PlottingWindow(object):
         self.controller.plot(False, self.controller.compare_rk, "Analytical - RK [nuclei]", "RK Comparison")
 
     def cancel_clicked(self):
-        self.PlottingWindow.close()
+        self.close()
